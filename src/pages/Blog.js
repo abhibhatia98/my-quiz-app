a60295d63d3001736dd8520c7ddb7caaa199a25a
abhishek <abc@gmail.com>
2023-02-06 13:48:47 +0530

    fixed blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -8,14 +8,15 @@ import { useParams } from 'react-router';
 function Blog() {
     const { id } = useParams();
     let blog = blogList.find((blog) => blog.id === parseInt(id));
-    return ( 
-        <div className='container my-3' style={{ paddingTop: '50px',paddingLeft:'110px', width:'80%'}}> 
-            {/* <Link className='blog-goBack' to='/'>
-                <span> &#8592;</span> <span>Go Back</span>
-            </Link> */}
-            <h1 style={{paddingTop: '50px' }}>{blog.title}</h1>
-            <p style={{paddingTop: '20px',color:'rgba(0, 0, 0, 0.4)'}}>{blog.description}</p>
-            <img src={blog.cover} style={{ width: '80%', height: '300px', alignItems:'center',paddingTop:'30px' }} alt='cover' />
+    return (
+        <div className='container my-3' style={{ paddingTop: '50px', paddingLeft: '110px', width: '80%' }}>
+            <h1 style={{ paddingTop: '50px' }}>{blog.title}</h1>
+            <p style={{ paddingTop: '20px', color: 'rgba(0, 0, 0, 0.4)' }}>{blog.description}</p>
+            <div className='d-flex-row-reverse' style={{ paddingLeft: '0%' }}>
+                <p className='d-fill' style={{ color: 'rgba(0, 0, 0, 0.4)', fontWeight: 'bolder' }}>{blog.authorName}</p>
+                <p className='d-fill' style={{ color: 'rgba(0, 0, 0, 0.4)', fontWeight: 'bolder', alignContent:'right' }}>21/06/2020</p>
+            </div>
+            <img src={blog.cover} style={{ width: '100%', height: '320px', alignItems: 'center', paddingTop: '10px' }} alt='cover' />
             <div className='container my-2'>
                 <p className='blog-desc'>{blog.fullContent}</p>
             </div>
